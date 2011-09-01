dce7ec06697cb5c85752416fce4c189d502f576f
Joseph Turner <turner@miserware.com>
2011-09-01 10:51:21 -0400

    Reformat and comments

--- BayesModel.py
+++ BayesModel.py
@@ -8,42 +8,53 @@ class BayesModel:
         self.word_probs = {}
 
     def add_document(self, document, judgement):
+        # Coerce judgement (currently only booleans) to int for indexing
+        #   TODO add capability for multi-judgement
         index = int(judgement)
+        # Increment # docs with judgement j
         self.docs[index] += 1
         words = document.split()
+        # Increment position totals for docs of judgement j
         self.word_totals[index] += len(words)
+        # Update word occurences
         for word in words:
             if word not in self.word_occs:
                 self.word_occs[word] = [0, 0]
             self.word_occs[word][index] += 1
+        # Rebuild probabilties from occurances
+        #   If a lot of documents are going to be trained at once, this should be delayed
         self.rebuild_probs()
 
     def rebuild_probs(self):
-        # P(word_k|v_j) = (n_k + 1)/(n + |vocab|)
-        # n = nr docs with judgement v_j
-        # n_k nr docs word k appears in with judgement v_j
+        # Init probabilities
         for word in self.word_occs:
             self.word_probs[word] = [0, 0]
         for t in [0, 1]:
+            # Estimate P(word_k|v_j) = (n_k + 1)/(n + |vocab|)
+            # TODO add mixins to determine whether to use fractions or floats for this 
+            #   (floats don't work on low numbers of training docs)
             for word in self.word_occs:
-                self.word_probs[word][t] = fractions.Fraction(self.word_occs[word][t] + 1, self.word_totals[t] + len(self.word_occs))
+                self.word_probs[word][t] = fractions.Fraction(
+                                                    self.word_occs[word][t] + 1, 
+                                                    self.word_totals[t] + len(self.word_occs)
+                                                )
         
     def judge(self, document):
-        # argmax P(v_j)MULT_k(P(word|v_j))
-        # P(v_j) = nr docs with judgement v_j / nr_documents
+        # classification = argmax_j P(v_j) * MULT_k(P(word|v_j))
         words = document.split()
+        # Only look at words that appear in the vocabulary
         test_words = filter(lambda x: x in self.word_probs, words)
         args = []
+        # Estimate the priors for P(v_j) * MULT_k(P(word|v_j))
         for j in [0, 1]:
             # estimate P(v_j)
             post = self.docs[j] / sum(self.docs)
+            # estimate MULT_k(P(word|v_j))
             word_probs = [self.word_probs[word][j] for word in test_words]
-            for w in word_probs:
-                post *= w
-            #post *= reduce(lambda x, y: x*y, word_probs)
+            post *= reduce(lambda x, y: x*y, word_probs)
             args.append(post)
+        # Argmax
         return args.index(max(args))
-        
 
 if __name__ == "__main__":
     model = BayesModel()
