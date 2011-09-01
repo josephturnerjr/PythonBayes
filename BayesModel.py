ba3bb21986caa61441385faad9e69b936e88e687
Joseph Turner <turner@miserware.com>
2011-09-01 10:20:27 -0400

    Added a preliminary judgement function, though it winds up with floats that are too small.

--- BayesModel.py
+++ BayesModel.py
@@ -17,30 +17,40 @@ class BayesModel:
         self.rebuild_probs()
         print self.word_probs
 
-    def judge(self, document):
-        words = document.split()
-        # argmax P(v_j)MULT_k(P(word|v_j))
-        # P(v_j) = nr docs with judgement v_j / nr_documents
-        P_v_j = [self.docs[x] / sum(self.docs) for x in [0,1]]
-        test_words = filter(lambda x: x in self.word_probs, words)
-        
-
     def rebuild_probs(self):
         # P(word_k|v_j) = (n_k + 1)/(n + |vocab|)
         # n = nr docs with judgement v_j
         # n_k nr docs word k appears in with judgement v_j
-        print self.word_occs
         for word in self.word_occs:
             self.word_probs[word] = [0, 0]
-        print self.word_probs
         for t in [0, 1]:
             for word in self.word_occs:
                 self.word_probs[word][t] = float(self.word_occs[word][t] + 1) / float(self.word_totals[t] + len(self.word_occs))
+        
+    def judge(self, document):
+        # argmax P(v_j)MULT_k(P(word|v_j))
+        # P(v_j) = nr docs with judgement v_j / nr_documents
+        words = document.split()
+        test_words = filter(lambda x: x in self.word_probs, words)
+        args = []
+        for j in [0, 1]:
+            # estimate P(v_j)
+            post = self.docs[j] / sum(self.docs)
+            word_probs = [self.word_probs[word][j] for word in test_words]
+            print j, word_probs
+            for w in word_probs:
+                post *= w
+                print post
+            #post *= reduce(lambda x, y: x*y, word_probs)
+            args.append(post)
+        print args
+        return args.index(max(args))
         
 
 if __name__ == "__main__":
     model = BayesModel()
     with open('d1.txt', 'r') as doc:
         b = doc.read()
-        model.add_document(b, True)
+    model.add_document(b, True)
+    print model.judge(b)
         
